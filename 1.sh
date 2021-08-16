38a41552f6a27eccb0415975652c144ca38d194c
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-08-16 18:32:45 +0800

    script: remove warning

--- 1.sh
+++ 1.sh
@@ -5,6 +5,7 @@ OBJDUMP="/home/zqm/disk-1/android/compiler/gcc-4.9-n/bin/aarch64-linux-android-o
 for i in `ls WORKING/`; do
 	git checkout script.py
 	grep -B10 "Parcel aPStack" DATA/${i}.c | grep "//" > DATA/${i}_f
+	sed -i '/WARNING:/d' DATA/${i}_f
 	$OBJDUMP -d WORKING/${i} > DATA/${i}.asm
 	sed -i "s|THEASMFILE|DATA/${i}.asm|g" script.py
 	sed -i "s|THEFUNCFILE|DATA/${i}_f|g" script.py
