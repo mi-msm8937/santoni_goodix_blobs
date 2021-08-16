d78a30e9274daa6404d7324f1855f1c1d96c3ff5
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-08-16 18:18:09 +0800

    import scripts

---
+++ 1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -xe
+
+OBJDUMP="/home/zqm/disk-1/android/compiler/gcc-4.9-n/bin/aarch64-linux-android-objdump"
+
+for i in `ls WORKING/`; do
+	git checkout script.py
+	grep -B10 "Parcel aPStack" DATA/${i}.c | grep "//" > DATA/${i}_f
+	$OBJDUMP -d WORKING/${i} > DATA/${i}.asm
+	sed -i "s|THEASMFILE|DATA/${i}.asm|g" script.py
+	sed -i "s|THEFUNCFILE|DATA/${i}_f|g" script.py
+	sed -i "s|THEOUTFILE|WORKING/${i}|g" script.py
+	python3 script.py
+done
