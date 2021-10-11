cdd02fb10eeedf5c8e326cd4c9091289890736bb
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-10-11 16:56:10 +0800

    set patchelf and drop libshims_binder.so

--- my-patchelf-fixup.sh
+++ my-patchelf-fixup.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-PATCHELF=""
+PATCHELF="~/bin/patchelf-0_9"
 
 $PATCHELF --remove-needed libbacktrace.so bin/gx_*
 $PATCHELF --remove-needed libunwind.so bin/gx_*
 
 #$PATCHELF --add-needed liblog.so bin/gx_*
-#$PATCHELF --add-needed fakelogprint.so bin/gx_*
-
-$PATCHELF --add-needed libshims_binder.so lib64/libfpservice.so
+$PATCHELF --add-needed fakelogprint.so bin/gx_*
 
 sed -i 's|libandroid_runtime.so|libshims_android.so\x00\x00|g' lib64/hw/fingerprint.goodix.so
